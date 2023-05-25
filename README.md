38b4c7ca7b7d993d685aace1995ded3afd9fa15f
Louis Antolin <ragnaman.exe@gmail.com>
2023-05-25 13:10:40 -0700

    update code

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # 04 Web APIs Code Quiz
+## Description
+
+The project is to build a webpage that runs a quiz game. The quiz game will prompt the user to answer some questions and will be scored out of a 1000 based on thier answers and how fast they complete it
+
+## Installation
+To start this game, you simple load the page and click start. Once you do it will start a timer and you will be prompted with questions that need to be answered by clicking the buttons. Please note that choosing a wrong answer or CLICKING ANYTHING but the answer itself will count as a wrong answer and will subract 20 from the timer. The game ends once all questions have been answered and the game will display your score.
+
+Note: Due to time constraints, I was unable to complete the critiria to save initials and the score into local memory.
+
+## Usage
+
+You can use this code to look at how the timer behaves. Also one can see the event listeners and how using event listeners requires chaining functions so that each event listeners is used to chain onto the next so that all the listeners don't activiate all at once.
+
+## Credits
+For this challenge assignment, I used code from the miniproject from week 4 Web API section. It was used to set up the timer, start button listeners, and a foundation to set up the page itself in order to create and remove buttons to be used in the quiz game.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,18 @@
 
-//var wordBlank = document.querySelector(".word-blanks");
-//var win = document.querySelector(".win");
-//var lose = document.querySelector(".lose");
+// global variables for timer, start buttone and game complete.
 var timerElement = document.querySelector(".timer-count");
 var startButton = document.querySelector(".start-button");
-//var option1Button = document.querySelector(".option1-button");
-//var option2Button = document.querySelector(".option2-button");
-//var option3Button = document.querySelector(".option3-button");
-//var option4Button = document.querySelector(".option4-button");
 var quizComplete = false;
-
-
 var timer;
 var timerCount;
 var score;
 
+//array with all the questions, options, and answers
 var answerSheet = [{question:"Commonly used data types DO NOT include", 
              options: ["Strings", "Booleans", "Alerts", "Numbers"],
              answer: "Alerts"},
             {question:"The condition in an if/else statement is in eclosed within _____.",
-            options: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
+            options: ["Quotes", "Curly brackets", "Parentheses", "Square\nbrackets"],
             answer: "Parentheses"},
             {question:"Arrays in Javascript can be used to store _____.",
             options: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
@@ -28,30 +21,22 @@ var answerSheet = [{question:"Commonly used data types DO NOT include",
             options: ["Commas", "Curly brackets", "Quotes", "Parentheses"],
             answer: "Quotes"},
             {question:"A useful tool used during development and debugging for printing content to the debugger is:", 
-            options: ["Javascript", "Terminal/bash", "For loops", "Console.log"],
+            options: ["Javascript", "Terminal/ bash", "For loops", "Console.log"],
             answer: "Console.log"}];
 
-//answerSheet[1].question;
-//answerSheet[1].options[0];
 
-//global values
+
+//global values to use when making each question page
 var opt1;
 var opt2;
 var opt3;
 var opt4;
+var result;
 var questionObj;
 var playerPush;
 
 var quizGameEL= document.getElementById("quizGame");
 var playerAnswer = null;
-
-
-// The init function is called when the page loads 
-function init() {
-  getWins();
-  getlosses();
-}
-
 
 // The startGame function is called when the start button is clicked
 function startGame() {
@@ -59,87 +44,175 @@ function startGame() {
   // Prevents start button from being clicked when round is in progress
   startButton.disabled = true;
   startButton.remove();
-  //renderBlanks()
   startTimer();
   startQuiz();
 }
 
-//starts up the quiz
+//starts up the quiz by running setupQuestion which chains into a series of setupQuestion calls
 function startQuiz(){
 
     setupQuestion(answerSheet, 0);
-    console.log(playerAnswer);
-    
-    
-   // setupQuestion(answerSheet, 1);
-
-   // quizGameEL.onclick=playerInput;
-
-t
-
+   
 }
 
 
 //intiializes the question and answer buttons.
 function setupQuestion(answerSheet,index)
 {
-questionObj = document.createElement("h1");
-questionObj.textContent = answerSheet[index].question;
-questionObj.setAttribute("style", "text-align: center");
-document.getElementById("quizGame").appendChild(questionObj);
+    questionObj = document.createElement("h1");
+    questionObj.textContent = answerSheet[index].question;
+    questionObj.setAttribute("style", "text-align: center");
+    document.getElementById("quizGame").appendChild(questionObj);
 
-opt1 = document.createElement("button");
-opt1.textContent = answerSheet[index].options[0];
-opt1.setAttribute("style", "text-align: center","display: flex",
-"justify-content: center","align-items:center");
-document.getElementById("quizGame").appendChild(opt1);
+    opt1 = document.createElement("button");
+    opt1.textContent = answerSheet[index].options[0];
+    opt1.setAttribute("style", "text-align: center","display: flex",
+    "justify-content: center","align-items:center");
+    document.getElementById("quizGame").appendChild(opt1);
 
-opt2 = document.createElement("button");
-opt2.textContent = answerSheet[index].options[1];
-opt2.setAttribute("style", "text-align: center","display: flex",
-"justify-content: center","align-items:center");
-document.getElementById("quizGame").appendChild(opt2);
+    opt2 = document.createElement("button");
+    opt2.textContent = answerSheet[index].options[1];
+    opt2.setAttribute("style", "text-align: center","display: flex",
+    "justify-content: center","align-items:center");
+    document.getElementById("quizGame").appendChild(opt2);
 
-opt3 = document.createElement("button");
-opt3.textContent = answerSheet[index].options[2];
-opt3.setAttribute("style", "text-align: center","display: flex",
-"justify-content: center","align-items:center");
-document.getElementById("quizGame").appendChild(opt3);
+    opt3 = document.createElement("button");
+    opt3.textContent = answerSheet[index].options[2];
+    opt3.setAttribute("style", "text-align: center","display: flex",
+    "justify-content: center","align-items:center");
+    document.getElementById("quizGame").appendChild(opt3);
 
-opt4 = document.createElement("button");
-opt4.textContent = answerSheet[index].options[3];
-opt4.setAttribute("style", "text-align: center","display: flex",
-"justify-content: center","align-items:center");
-document.getElementById("quizGame").appendChild(opt4);
+    opt4 = document.createElement("button");
+    opt4.textContent = answerSheet[index].options[3];
+    opt4.setAttribute("style", "text-align: center","display: flex",
+    "justify-content: center","align-items:center");
+    document.getElementById("quizGame").appendChild(opt4);
 
-quizGameEL = document.getElementById("quizGame");
-
-
-
-//quizGameEL.onclick=playerInput;
-quizGameEL.addEventListener("click", playerInput);
-console.log(quizGameEL);
-
+    //checks to see which question is being passed down
+    //once the first question is answered, it will create a chain of events
+    //in which each question is deleted and refills the page with the next until it finishes.
+    if(index == 0)
+    {
+        quizGameEL.onclick=playerInput0;
+    }
+    if(index == 1)
+    {
+         quizGameEL.onclick=playerInput1;
+    }
+    if(index == 2)
+    {
+        quizGameEL.onclick=playerInput2;
+    }
+    if(index == 3)
+    {
+        quizGameEL.onclick=playerInput3;
+    }
+    if(index == 4)
+    {
+        quizGameEL.onclick=playerInput4;
+    }
 
 }
 
-//checks answer and removes elements made by the function setupQuestion
-function playerInput(event)
+//checks players answer and removes elements made by the function setupQuestion
+//each question click will rest and trigger the next one until it finishes
+function playerInput0(event)
 {
-    //event.stop
-    //event.stopPropagation();
     playerPush = event.target;
-    //console.log(temp);
+    playerAnswer =playerPush.textContent;
+    console.log(playerAnswer);
+    //penalize the player for clicking a wrong answer
+    if(playerAnswer !== answerSheet[0].answer && playerAnswer !== null)
+    {
+        timerCount = timerCount-20;
+       
+       
+    }
+    //removes elements made by setupQuestion
+    resetQuestion();
+    //trigger the next question until it finishes
+    setupQuestion(answerSheet, 1);
+}
+
+function playerInput1(event)
+{
+    
+    playerPush = event.target;
+    playerAnswer =playerPush.textContent;   
+    if(playerAnswer !== answerSheet[1].answer && playerAnswer !== null)
+    {
+        timerCount = timerCount-20;
+    }
+    resetQuestion();
+    setupQuestion(answerSheet, 2);
+}
+
+function playerInput2(event)
+{
+    
+    playerPush = event.target;
     playerAnswer =playerPush.textContent;
     console.log(playerAnswer);
    
-    //if(playerAnswer !== answerSheet[index].answer && playerAnswer !== null)
-    //{
-        //timerCount = timerCount-20;
-    //}
+    if(playerAnswer !== answerSheet[2].answer && playerAnswer !== null)
+    {
+        timerCount = timerCount-20;
+    }
     resetQuestion();
+    setupQuestion(answerSheet, 3);
 }
 
+function playerInput3(event)
+{
+    
+    playerPush = event.target;
+    playerAnswer =playerPush.textContent;
+    console.log(playerAnswer);
+   
+    if(playerAnswer !== answerSheet[3].answer && playerAnswer !== null)
+    {
+        timerCount = timerCount-20;
+    }
+    resetQuestion();
+    setupQuestion(answerSheet, 4);
+}
+
+function playerInput4(event)
+{
+   
+    playerPush = event.target;
+    playerAnswer =playerPush.textContent;
+    console.log(playerAnswer);
+   
+    if(playerAnswer !== answerSheet[4].answer && playerAnswer !== null)
+    {
+        timerCount = timerCount-20;
+    }
+    resetQuestion();
+    //once the code reaches here, the game is done
+    quizComplete = true;
+    score = timerCount;
+    timerCount = 0;
+    console.log(score);
+    //hides the timer
+    timerElement.setAttribute("style", "visibility: hidden");
+    finalScore(score);
+}
+
+
+//displays the final score
+function finalScore(temp){
+    var tempObj = document.createElement("h1");
+    tempObj.textContent = "Your final score is " + temp;
+    console.log(tempObj.textContent);
+    tempObj.setAttribute("style", "text-align: center","display: flex",
+    "justify-content: center","align-items:center");
+    document.getElementById("quizGame").appendChild(tempObj);
+    
+}
+
+
+//rests quizgame after every question
 function resetQuestion()
 {
     opt1.remove();
@@ -152,23 +225,6 @@ function resetQuestion()
 
 
 
-// The winGame function is called when the win condition is met
-/*
-function winGame() {
-  wordBlank.textContent = "YOU WON!!!🏆 ";
-  winCounter++
-  startButton.disabled = false;
-  setWins()
-}
-
-// The loseGame function is called when timer reaches 0
-function loseGame() {
-  wordBlank.textContent = "GAME OVER";
-  loseCounter++
-  startButton.disabled = false;
-  setLosses()
-}
-*/
 // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
 function startTimer() {
   // Sets timer
@@ -184,7 +240,7 @@ function startTimer() {
       }
     }
     // Tests if time has run out
-    if (timerCount === 0) {
+    if (timerCount <= 0) {
       // Clears interval
       score = timerCount;
       clearInterval(timer);
@@ -192,125 +248,6 @@ function startTimer() {
   }, 1000);
 }
 
-// Creates blanks on screen
-/*function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
-  // Uses loop to push blanks to blankLetters array
-  for (var i = 0; i < numBlanks; i++) {
-    blanksLetters.push("_");
-  }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
-//}
-*/
-// Updates win count on screen and sets win count to client storage
-//function setWins() {
-  //win.textContent = winCounter;
-  //localStorage.setItem("winCount", winCounter);
-//}
-
-// Updates lose count on screen and sets lose count to client storage
-//function setLosses() {
-  //lose.textContent = loseCounter;
-  //localStorage.setItem("loseCount", loseCounter);
-//}
-
-// These functions are used by init
-/*function getWins() {
-  // Get stored value from client storage, if it exists
-  var storedWins = localStorage.getItem("winCount");
-  // If stored value doesn't exist, set counter to 0
-  if (storedWins === null) {
-    winCounter = 0;
-  } else {
-    // If a value is retrieved from client storage set the winCounter to that value
-    winCounter = storedWins;
-  }
-  //Render win count to page
-  win.textContent = winCounter;
-}
-*/
-/*
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
-*/
-/*
-function checkWin() {
-  // If the word equals the blankLetters array when converted to string, set isWin to true
-  if (chosenWord === blanksLetters.join("")) {
-    // This value is used in the timer function to test if win condition is met
-    isWin = true;
-  }
-}
-*/
-
-// Tests if guessed letter is in word and renders it to the screen.
-/*function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
-  }
-}
-*/
-
-// Attach event listener to document to listen for key event
-/*document.addEventListener("keydown", function(event) {
-  // If the count is zero, exit function
-  if (timerCount === 0) {
-    return;
-  }
-  // Convert all keys to lower case
-  var key = event.key.toLowerCase();
-  var alphabetNumericCharacters = "abcdefghijklmnopqrstuvwxyz0123456789 ".split("");
-  // Test if key pushed is letter
-  if (alphabetNumericCharacters.includes(key)) {
-    var letterGuessed = event.key;
-    checkLetters(letterGuessed)
-    checkWin();
-  }
-});
-*/
 
 // Attach event listener to start button to call startGame function on click
 startButton.addEventListener("click", startGame);
-
-
-//ADD back later to perform score keeping
-// Calls init() so that it fires when page opened
-//init();
-
-// Bonus: Add reset button
-//var resetButton = document.querySelector(".reset-button");
-/*
-function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
-}
-*/
-// Attaches event listener to button
-//resetButton.addEventListener("click", resetGame);
